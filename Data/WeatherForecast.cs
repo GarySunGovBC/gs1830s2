02ea8b2f0572123684c9cd4a7e5e9ad4d645bbe5
Gary Sun <Gary.Sun@gov.bc.ca>
2022-05-20 16:09:28 -0700

    first time commit

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace gs1830s2.Data;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
